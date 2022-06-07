0e56ca4fa58123d9b3d6274c8c855fae31927d36
liuyuntian <liuyuntian110@126.com>
2022-06-08 02:13:48 +0800

    feat: contract integration

--- src/components/Thirdpage/Thirdpage.js
+++ src/components/Thirdpage/Thirdpage.js
@@ -6,10 +6,12 @@ import Socialcards from './Social_cards/SocialCards.js'
 import Audited from './Audited/Audited.js'
 import Featured from './Featured/Featured.js'
 import Footer from './Footer/Footer.js'
+import background from "./background.svg"
 
 const Thirdpage = (props) => {
   return (
     <div className='Thirdpage'>
+        <img className="background-img" src={background}/>
         <SPAcards burnt={props.burnt} locked={props.locked} buyBack={props.buyBack}/>
         <Spartan/>
         <div className='joinspadiv'><h1 className='joinspa'><b> Join the SPArtan army!!</b></h1></div>
