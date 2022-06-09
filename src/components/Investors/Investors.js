08e865bfe1d6b03e57549a62c7f872802a90f2f4
liuyuntian <liuyuntian110@126.com>
2022-06-09 20:58:22 +0800

    feat: desktop and tablet

--- src/components/Investors/Investors.js
+++ src/components/Investors/Investors.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './investors.css'
 import polychain from './polychain.svg'
-import jump from './jumptrading.svg'
-import alameda from './alameda.svg'
+import jump from './jumptrading.png'
+import alameda from './alameda.png'
 import amber from './amber.svg'
 import outlier from './outlier.svg'
 import steve from './steve.svg'
@@ -15,8 +15,8 @@ const Investors = () => {
                   <span className='text'>Backed by</span>
               </div>
               <div><img src={polychain} alt="" /></div>
-              <div><img src={jump} alt="" /></div>
-              <div><img src={alameda} alt="" /></div>
+              <div><img className="jump_img" src={jump} alt="" /></div>
+              <div><img className="alameda_img" src={alameda} alt="" /></div>
               <div><img src={amber} alt="" /></div>
               <div><img src={outlier} alt="" /></div>
               <div><img src={steve} alt="" /></div>
