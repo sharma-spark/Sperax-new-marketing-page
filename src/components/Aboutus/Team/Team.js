45057729eb3fce53643375e1e1cd17830a7ef1c0
Ayush <ayushayushayush007@gmail.com>
2022-06-28 12:47:59 +0530

    bugs2fixed

--- src/components/Aboutus/Team/Team.js
+++ src/components/Aboutus/Team/Team.js
@@ -15,10 +15,10 @@ const Team = () => {
     <div className='team'>
         <Personcard image={rey} name={"Rey Maximo"} desg={"Engineer"} descp={data.rey} linkedin={"https://www.linkedin.com/in/maximo/"}/>
         <Personcard image={nicolas} name={"Nicolas Andreoulis"} desg={"Research"} descp={data.nicolas}/>
-        <Personcard image={subh} name={"Subhajit Ray"} desg={"Product"} descp={data.subhajit} twitter={"https://twitter.com/subhajit_da"} linkedin={"https://www.linkedin.com/in/subhajit1/"}/>
+        <Personcard image={subh} name={"Subhajit Ray"} desg={"Product"} descp={data.subhajit}  linkedin={"https://www.linkedin.com/in/subhajit1/"}/>
         <Personcard image={joe} name={"Joe Yu"} desg={"Engineer"} descp={data.joe} linkedin={"https://www.linkedin.com/in/zhou-y-400894184/"}/>
         <Personcard image={alec} name={"Alec Shaw"} desg={"Marketing and BD"} descp={data.alec} twitter={"https://twitter.com/Alec_Sperax"} linkedin={"https://www.linkedin.com/in/alectshaw/"}/>
-        <Personcard image={atreyee} name={"Atreyee Bhaumik"} desg={"Product"} descp={data.atreyee} linkedin={"https://www.linkedin.com/in/atreyeebhaumik/"} twitter={"https://twitter.com/Atreyee_Sperax"}/>
+        <Personcard image={atreyee} name={"Atreyee Bhaumik"} desg={"Product"} descp={data.atreyee} linkedin={"https://www.linkedin.com/in/atreyeebhaumik/"} />
         <Personcard image={david} name={"David Zhao"} desg={"Engineer"} descp={data.david} linkedin={"https://www.linkedin.com/in/david-zhao-b40116112/"} twitter={"https://twitter.com/DSperax"}/>
     </div>
   )
