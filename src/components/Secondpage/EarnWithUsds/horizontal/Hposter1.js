a1275cb701d8e856bee9f52e3e7ff26d26cbc82d
Ayush <ayushayushayush007@gmail.com>
2022-07-01 19:20:10 +0530

    bugs9fixed

--- src/components/Secondpage/EarnWithUsds/horizontal/Hposter1.js
+++ src/components/Secondpage/EarnWithUsds/horizontal/Hposter1.js
@@ -12,13 +12,13 @@ const Hposter1 = () => {
             <p>Earn up to 11% APY in auto-yield on the USDs held in your wallet</p>
         </div>
         <div className='alinks'>
-            <a onClick={() => setShow(true)}><span className='at'>Buy USDs </span>  <img src={arrow} alt="" /></a>
+            <a href='https://global.transak.com/?apiKey=6f11a8a6-81e6-4639-915c-13bac6fba229&cryptoCurrencyList=USDS,ETH,USDC,SPA&defaultNetwork=arbitrum&networks=ethereum,arbitrum&defaultCryptoCurrency=USDS'><span className='at'>Buy USDs </span>  <img src={arrow} alt="" /></a>
             <a href="https://app.sperax.io/mint"><span className='at'>Mint USDs </span> <img src={arrow} alt="" /></a>
         </div>
         <div className='postimg'>
             <img src={hposter1img} alt="" />
         </div>
-        <Modal show={show} onHide={() => setShow(false)} type={'USDS'} />
+        {/* <Modal show={show} onHide={() => setShow(false)} type={'USDS'} /> */}
     </div>
   )
 }
--- src/components/Secondpage/Speraxeco/Spaeco.js
+++ src/components/Secondpage/Speraxeco/Spaeco.js
@@ -10,9 +10,9 @@ const Spaeco = () => {
             <span>SPA is the value accrual and governance token for the<span className='in'> Sperax ecosystem </span></span>
         </div>
         <div className='getspa'>
-            <a className="darkBtn" onClick={() => {setShow(true)}}>Get SPA →</a>
+            <a className="darkBtn" href='https://global.transak.com/?apiKey=6f11a8a6-81e6-4639-915c-13bac6fba229&cryptoCurrencyList=USDS,ETH,USDC,SPA&defaultNetwork=arbitrum&networks=ethereum,arbitrum&defaultCryptoCurrency=SPA'>Get SPA →</a>
         </div>
-        <Modal show={show} onHide={() => setShow(false)} type={'SPA'} />
+        {/* <Modal show={show} onHide={() => setShow(false)} type={'SPA'} /> */}
     </div>
   )
 }
