e1ac1d4e0b17f4b593a824a683fe22d2a6ecd1b8
Tangmou <my249645546@163.com>
2022-07-06 08:45:31 +0800

    feat. integrated Twitter api

--- src/socialapi/twitterapi.js
+++ src/socialapi/twitterapi.js
@@ -1,23 +1,19 @@
 import axios from "axios";
 export const getTwittercount = () => {
-  return new Promise((resolve, reject) => {
-    console.log("check");
-    let token = "AAAAAAAAAAAAAAAAAAAAADS5eQEAAAAACZSLhvoGJtc1xv%2BwJpXGLovn1iA%3D2ugmA1kESPkoLeG9izORyvNdhi7nOZqt2n5MnNepD20AGtvr2R";
-      axios({
-        method: 'get',
-        url: "/twitter-api/2/users/1424805429754810388?user.fields=public_metrics",
-        headers: {
-            'Content-Type': 'application/json',
-            "access-control-allow-origin" : "*",
-            "Authorization": `Bearer ${token}`
-        },
-    }).then(response => {
-<<<<<<< HEAD
-         console.log(response.data.data.public_metrics.followers_count);
-         resolve(response.data.data.public_metrics.followers_count)
-=======
-         console.debug(`twitter api response:`, response);
->>>>>>> 75cd10d490a919b98ba5c6c9857d6b2825f14958
+    return new Promise((resolve, reject) => {
+        console.log("check");
+        let token = "AAAAAAAAAAAAAAAAAAAAADS5eQEAAAAACZSLhvoGJtc1xv%2BwJpXGLovn1iA%3D2ugmA1kESPkoLeG9izORyvNdhi7nOZqt2n5MnNepD20AGtvr2R";
+        axios({
+            method: 'get',
+            url: "/twitter-api/2/users/1424805429754810388?user.fields=public_metrics",
+            headers: {
+                'Content-Type': 'application/json',
+                "access-control-allow-origin" : "*",
+                "Authorization": `Bearer ${token}`
+            },
+        }).then(response => {
+            console.log(response.data.data.public_metrics.followers_count);
+            resolve(response.data.data.public_metrics.followers_count)
+        });
     });
-  });
 };
