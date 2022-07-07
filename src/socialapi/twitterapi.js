069abdedc5c9fa477be61b95d85ce6d8fb242860
Ayush <ayushayushayush007@gmail.com>
2022-07-07 18:58:27 +0530

    issues

--- src/socialapi/twitterapi.js
+++ src/socialapi/twitterapi.js
@@ -1,22 +1,5 @@
 import axios from "axios";
 export const getTwittercount = () => {
-<<<<<<< HEAD
-  return new Promise((resolve, reject) => {
-    console.log("check");
-    let token = "AAAAAAAAAAAAAAAAAAAAADS5eQEAAAAACZSLhvoGJtc1xv%2BwJpXGLovn1iA%3D2ugmA1kESPkoLeG9izORyvNdhi7nOZqt2n5MnNepD20AGtvr2R";
-      axios({
-        method: 'get',
-        url: "/twitter-api/2/users/1424805429754810388?user.fields=public_metrics",
-        headers: {
-            'Content-Type': 'application/json',
-            "access-control-allow-origin" : "*",
-            "Authorization": `Bearer ${token}`
-        },
-    }).then(response => {
-         console.log(response.data.data.public_metrics.followers_count);
-         resolve(response.data.data.public_metrics.followers_count);
-
-=======
     return new Promise((resolve, reject) => {
         console.log("check");
         let token = "AAAAAAAAAAAAAAAAAAAAADS5eQEAAAAACZSLhvoGJtc1xv%2BwJpXGLovn1iA%3D2ugmA1kESPkoLeG9izORyvNdhi7nOZqt2n5MnNepD20AGtvr2R";
@@ -32,6 +15,5 @@ export const getTwittercount = () => {
             console.log(response.data.data.public_metrics.followers_count);
             resolve(response.data.data.public_metrics.followers_count)
         });
->>>>>>> 470a1a018877244e1db827861468b5427173bf5d
     });
 };
